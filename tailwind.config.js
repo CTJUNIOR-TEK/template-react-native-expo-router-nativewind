16f577004c5e28450943ff46e19f159fa4345aff
PedroSodreUFES <pedro.malini@edu.ufes.br>
2025-11-06 11:48:28 -0300

    feat: expliccando como configurar cores persdonalizadas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
+import { red } from 'react-native-reanimated/lib/typescript/Colors';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: {
+          500: '#FF0000',
+          400: '#FF4D4D',
+          300: '#FF9999',
+        },
+        'sodras': "#00ffaa"
+      },
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      },
+      fontSize: {
+        sm: "14px",
+        base: "16px",
+        lg: "20px",
+        xl: "24px",
+      }
+    },
   },
   plugins: [],
 }
